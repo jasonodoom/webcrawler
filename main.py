4d982b7d99f0147e3696eaf30edbdc969c17cb20
Jason Odoom <jason@ohdoom.net>
2016-03-01 01:21:46 -0500

    I know it looks like a third trimester abortion but atleast it downloads web pages now. I will return for more fun.

--- main.py
+++ main.py
@@ -1,43 +1,60 @@
 #!/usr/bin/python
 
+import urllib.request
 from urllib.request import urlopen 
 from lxml import etree
 from lxml import html
 
 
 def crawl():
-    '''Crawls given webpages searching for keywords. If keywords are found
-in the webpage, it will write to disk. Recommended use with Wikipedia.'''
+    '''Crawls given webpages, saves to disk and searches them. Recommended use with WikiPedia.'''
     seedOne = input("Please enter  the first seed URL: \n")
     print("You entered: \n" + seedOne)
     seedTwo= input("\nPlease enter the second seed URL: \n")
     print("You entered:\n " +  seedTwo)
     keywords=input("\nPlease enter keywords related to your link: ")
-    print('\nPlease sit tight, this will take a while to process......')
-    dictionary = keywords.split()
+    print('\nPlease sit tight while I process your request......')
+    dictionary = keywords.lower()
     htmlOne  = urlopen(seedOne).read()
     htmlTwo = urlopen(seedTwo).read()
     treeOne = etree.fromstring(htmlOne) # Parse the HTML
     treeTwo = etree.fromstring(htmlTwo)
-    seedList=[]
-    for x in treeOne.iterfind(".//a"):
-        seedList.append(seedOne)
-        seedList.append((x.get("href")))
-        for x in treeTwo.iterfind(".//a"):
-            seedList.append(seedTwo)
-            seedList.append((x.get("href")))
-            filew = open("1.html", "a+")
-            for i, line in enumerate(seedList):
-                with open("1_%i.html" %i, "a+") as f: #writing to multiple files that are created instantly with this naming scheme . 
-                    f.write((repr(seedList )))
-                    if i >= 500: #Stops creating files at this number.
-                        break
-                    for word in f:
-                        if word in keywords:
-                            dictionary.append(word)
-                            
-                                                 
-            
+    linkList=[]
+    with urllib.request.urlopen(seedOne) as response:
+        seedOnehtml = response.read()
+        for x in seedOnehtml:
+            holdOneTree = treeOne.iterfind(".//a")
+    with urllib.request.urlopen(seedTwo) as response:
+        seedTwohtml = response.read()
+        for x in seedTwohtml:
+            holdTwoTree = treeTwo.iterfind(".//a")
+        sourceCombo = seedOnehtml + seedTwohtml
+        for i, line in enumerate(sourceCombo):
+            with open("1_%i.html" %i, "a+") as f: #writing to multiple files that are cre*typedated instantly with this naming scheme .
+                f.write((repr(sourceCombo)))
+                if i >= 500: #Stops generating  files at this number.
+                    break
+                for x in holdOneTree:
+                    linkList.append(seedOne)
+                    linkList.append((x.get("href")))
+                    break
+                for x in holdTwoTree:
+                    linkList.append(seedTwo)
+                    linkList.append((x.get("href")))
+                    break
+                for i, line in enumerate(linkList):
+                    with open("linkList.txt", "a+") as Fin:
+                        Fin.write((repr(str(linkList))))
+                        if i >= 1: #Stops creating files at this number.
+                            break
+                        with open('linkList.txt', 'r') as Fin:
+                            for line in Fin:
+                                if line in dictionary:
+                                    dictionary + line
+                                    print(dictionary)
+                       
+
+
 print(crawl())
- 
+
 
